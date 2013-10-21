ad8df0821418dbdc52294fa7a099e49a0e0d3325
Louis <cal@ubuntu.(none)>
2013-10-21 20:39:39 +0200

    NN's comments

--- NeuralNetwork/ANU.ml
+++ NeuralNetwork/ANU.ml
@@ -1,5 +1,6 @@
 class anu =
 object
+(* simulates a neuron, basically just a float, shouldn't have done that *)
   val mutable activation = 0.
   method chg a = activation <- a
   method get = activation
--- NeuralNetwork/training.ml
+++ NeuralNetwork/training.ml
@@ -1,7 +1,7 @@
-class training ann = 
+class training () = 
 object (self)
-val mutable anna  = new ann
 
+(* renvoie les ouputs du reseau de neurones pour chaque cas donné par les inputs *)
 method get_result inputs (anna:ann) =
   let result = Array.make_matrix (Array.length inputs) 1 1. in
   for i = 0 to (Array.length inputs)-1 do
@@ -9,9 +9,12 @@ method get_result inputs (anna:ann) =
     result.(i)<- anna#get_output 1;
   done;
     result
- 
-method t (mat:float array array) = 
-  let sol = Array.make_matrix (Array.length mat.(0)) (Array.length mat) mat.(0).(0) in
+
+ (* fait la transposée d'une matrice *)
+method t (mat:float array array) =
+  let width = Array.length mat.(0) 
+  and heigth = Array.length mat in
+  let sol = Array.make_matrix width heigth mat.(0).(0) in
   for i = 0 to (Array.length mat)-1 do
     for j = 0 to (Array.length mat.(0))-1 do
       sol.(j).(i) <- mat.(i).(j)
@@ -19,7 +22,7 @@ method t (mat:float array array) =
   done;
   sol
 
-
+(* fait la multiplacation d'une matrice et d'un vecteur *)
 method mult mat1 mat2 =
   let result = Array.make (Array.length mat1) 1. in
   for i = 0 to (Array.length mat1)-1 do
@@ -31,7 +34,7 @@ method mult mat1 mat2 =
     sol := 0.;
   done;
   result
-
+(* Calcul de l'erreur d'une hidden layer *)
 method delta mw error (activation:float array) = 
   let result = self#mult (self#t mw) error
   and sol = Array.make (Array.length activation) 1. in
@@ -40,6 +43,7 @@ method delta mw error (activation:float array) =
   done;
   sol
 
+(* fait une multiplacation de deux vecteur X,1 pour avoir une matrice X,X *)
 method mat_mult ar1 ar2 = 
   let sol = Array.make_matrix (Array.length ar1) (Array.length ar2) 1. 
   in
@@ -50,6 +54,7 @@ method mat_mult ar1 ar2 =
   done;
   sol
 
+(* Calcule l'erreur de l'ouput layer *)
 method first_error activation hopes  = 
   let result = Array.make (Array.length hopes) 1. in
   for i = 0 to Array.length result -1 do
@@ -57,6 +62,7 @@ method first_error activation hopes  =
   done;
   result
 
+(* met à jour les poids du NN *)
 method update_weights mw1 mw2 d1 d2  = 
   for i = 0 to Array.length d1 -1 do
     for j = 0 to Array.length d1.(0)-1 do
@@ -68,15 +74,15 @@ method update_weights mw1 mw2 d1 d2  =
       mw2.(i).(j+1) <- mw2.(i).(j+1) -. (d2.(i).(j)*.0.2);
     done;
   done;
-  (mw1,mw2)
-  
+
+(* somme membre à membre de deux vecteurs *)  
 method sum ar1 ar2 = 
   let sol = Array.make (Array.length ar1) 1. in
   for i = 0 to Array.length sol -1 do
     sol.(i) <- ar1.(i) +. ar2.(i)
   done;
   sol
-
+(* Somme membre à membre de deux matrics *)
 method sum_mat m1 m2 = 
   let sol = Array.make_matrix (Array.length m1) (Array.length m1.(0)) 0.
   in
@@ -86,7 +92,7 @@ method sum_mat m1 m2 =
     done;
   done;
   sol
-
+(* Multiplication membre à membre de deux vecteurs *)
 method test ar1 ar2 = 
   let sol = Array.make (Array.length ar2) 1. in
   for i = 0 to Array.length sol -1 do
@@ -94,6 +100,8 @@ method test ar1 ar2 =
   done;
   sol
 
+
+(* Divise l'erreur des couches internes par le nombre de cas d'entrainement *)
 method div_delta m m1 = 
   let sol = Array.make_matrix (Array.length m1) (Array.length m1.(0)) 0.
 in
@@ -103,37 +111,33 @@ in
     done;
   done;
   sol
+
+(* self explanatory *)
 method train (p:ann) inputs outputs =
-  let prout = Array.make_matrix 3 2 1.
-  and caca = Array.make 3 1.
+  let errors = Array.make_matrix 3 2 1.
   and delta1 =ref  (Array.make_matrix (Array.length (p#get 1)) (Array.length (p#get 1).(0)-1) 0.)
   and delta2 =ref  (Array.make_matrix (Array.length (p#get 2)) (Array.length (p#get 2).(0)-1) 0.)
   in
-  prout.(0) <- Array.make 1 0.;
-  prout.(1) <- Array.make 2 0.;
-  prout.(2) <- Array.make 2 0.;
+  errors.(0) <- Array.make 1 0.;
+  errors.(1) <- Array.make 2 0.;
+  errors.(2) <- Array.make 2 0.;
   for i = 0 to 100000 do
     for z = 0 to Array.length inputs -1 do
       p#forward_propagation inputs.(z) (p#get 1) (p#get 2);
-      (* je fait la somme trop tot, il faut calcucler les derivatives avant *)
-      prout.(0) <- self#first_error (p#get_output 1) (outputs.(z));
-      prout.(1) <- (self#delta (p#get 2) prout.(0) (p#get_output 0)) ;
-      prout.(2) <- (self#delta (p#get 1) prout.(1) inputs.(z));
-      delta1 := self#sum_mat (!delta1) (self#mat_mult (inputs.(z)) prout.(1));
-      delta2 := self#sum_mat (!delta2) (self#mat_mult (p#get_output 0) prout.(0));
-      (*for lo = 0 to (Array.length prout.(0))-1 do
-	print_float prout.(0).(lo);
-	print_newline();
-      done;*)
+      errors.(0) <- self#first_error (p#get_output 1) (outputs.(z));
+      errors.(1) <- (self#delta (p#get 2) errors.(0) (p#get_output 0)) ;
+      errors.(2) <- (self#delta (p#get 1) errors.(1) inputs.(z));
+      delta1 := self#sum_mat (!delta1) (self#mat_mult (inputs.(z)) errors.(1));
+      delta2 := self#sum_mat (!delta2) (self#mat_mult (p#get_output 0) errors.(0));
     done;
     delta1 := self#div_delta (Array.length inputs) !delta1;
     delta2 := self#div_delta (Array.length inputs) !delta2;
     self#update_weights (p#get 1) (p#get 2) !delta1 !delta2;
     delta1 := (Array.make_matrix (Array.length (p#get 1)) (Array.length (p#get 1).(0)-1) 0.);
     delta2 := (Array.make_matrix (Array.length (p#get 2)) (Array.length (p#get 2).(0)-1) 0.);
-    prout.(0) <- Array.make 1 0.;
-    prout.(1) <- Array.make 2 0.;
-    prout.(2) <- Array.make 2 0.;
+    errors.(0) <- Array.make 1 0.;
+    errors.(1) <- Array.make 2 0.;
+    errors.(2) <- Array.make 2 0.;
   done;
   for q = 0 to Array.length inputs - 1 do
     p#forward_propagation  inputs.(q) (p#get 1) (p#get 2);
@@ -143,89 +147,3 @@ method train (p:ann) inputs outputs =
   
 
 end;;
-let fonc (p:ann) (j:training) inputs outputs =
-  let prout = Array.make_matrix 3 2 1.
-  and caca = Array.make 3 1.
-  and delta1 =ref  (Array.make_matrix (Array.length (p#get 1)) (Array.length (p#get 1).(0)-1) 0.)
-  and delta2 =ref  (Array.make_matrix (Array.length (p#get 2)) (Array.length (p#get 2).(0)-1) 0.)
-  in
-  prout.(0) <- Array.make 1 0.;
-  prout.(1) <- Array.make 2 0.;
-  prout.(2) <- Array.make 2 0.;
-  for i = 0 to 100000 do
-    for z = 0 to Array.length inputs -1 do
-      p#forward_propagation inputs.(z) (p#get 1) (p#get 2);
-      (* je fait la somme trop tot, il faut calcucler les derivatives avant *)
-      prout.(0) <- self#first_error (p#get_output 1) (outputs.(z));
-      prout.(1) <- (self#delta (p#get 2) prout.(0) (p#get_output 0)) ;
-      prout.(2) <- (self#delta (p#get 1) prout.(1) inputs.(z));
-      delta1 := self#sum_mat (!delta1) (self#mat_mult (inputs.(z)) prout.(1));
-      delta2 := self#sum_mat (!delta2) (self#mat_mult (p#get_output 0) prout.(0));
-      (*for lo = 0 to (Array.length prout.(0))-1 do
-	print_float prout.(0).(lo);
-	print_newline();
-      done;*)
-    done;
-    delta1 := self#div_delta (Array.length inputs) !delta1;
-    delta2 := self#div_delta (Array.length inputs) !delta2;
-    j#update_weights (p#get 1) (p#get 2) !delta1 !delta2;
-    delta1 := (Array.make_matrix (Array.length (p#get 1)) (Array.length (p#get 1).(0)-1) 0.);
-    delta2 := (Array.make_matrix (Array.length (p#get 2)) (Array.length (p#get 2).(0)-1) 0.);
-    prout.(0) <- Array.make 1 0.;
-    prout.(1) <- Array.make 2 0.;
-    prout.(2) <- Array.make 2 0.;
-  done;
-  for q = 0 to Array.length inputs - 1 do
-    p#forward_propagation  inputs.(q) (p#get 1) (p#get 2);
-    p#get_result;
-  done;;
-  
-let p = new ann 2 2 1;;
-let j = new training 1 ;;
-let inputs = Array.make_matrix 4 2 1.;;
-p#get 1;;
-p#get 2;;
-inputs.(1).(1) <- 0.;;
-inputs.(2).(0) <- 0.;;
-inputs.(3).(0) <- 0.;;
-inputs.(3).(1) <- 0.;;
-let outputs = Array.make_matrix 4 1 1.;;
-outputs.(1).(0) <- 0.;;
-outputs.(2).(0) <- 0.;;
-outputs;;
-p#forward_propagation inputs.(1) (p#get 1) (p#get 2);;
-p#get_output 1;;
-p#get_output 0;;
-fonc p j inputs outputs;;
-let test  = Array.make 2 1.;;
-p#forward_propagation test (p#get 1) (p#get 2);;
-p#get_result;;
-let results = p#get_output 1;;
-let hopes = Array.make 1 1.;;
-let error1=j#first_error results hopes;;
-let error2 = j#delta (p#get 2) error1 (p#get_output 0);;
-let error3 = j#delta (p#get 1) error2 inputs.(2);;
-let prout = Array.make_matrix 3 1 1.;; 
-let caca = j#t (p#get 2);;
-let test = j#mult caca error1;;
-j#mult (j#t (p#get 1)) error2;;
-inputs.(0);;
-prout.(0) <- error1;;
-prout.(1) <- error2;;
-prout.(2) <- error3;;
-j#update_weights (p#get 1) (p#get 2) prout;;
-p#forward_propagation inputs.(0) (p#get 1) (p#get 2);;
-p#get_result;;
-prout.(2);;
-prout;;
-p#get 1;;
-p#get 2;;
-p#get_output 0;;
-p#get_output 1;;
-error1;;
-error2;;
-Array.length (p#get 0).(0) -1 ;;
-Array.length prout.(0) -1;;
-caca;;
-error1;;
-(p#get 2).(0);;
