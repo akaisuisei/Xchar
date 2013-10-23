703796f538ca5b52978ff4ccc23bd0a9efec882a
akaisuisei <bjaykamwa@hotmail.com>
2013-10-23 04:18:19 +0200

    Signed-off-by: akaisuisei <bjaykamwa@hotmail.com>

--- src/main.ml
+++ src/main.ml
@@ -27,53 +27,49 @@ let show img dst =
 
 let main ()=
 	begin
-  let diver = ref 0 in
-  let rotok= ref false and d_rot = ref 0 and b_show = ref false in
-  let range = Array.length Sys.argv in
-  for i = 1 to range do
-    match Sys.argv.(i) with
-      | "-r" -> if i + 1 < range then
-	  begin
-	    rotok := true;
-	    d_rot := int_of_string ( Sys.argv.(i+1) );
-	  end
-      | "-s" -> b_show := true
-      | _ -> diver := 1; 
-  done;
+	let diver = ref 0 and a = ref 0 in
+	let rotok= ref false and d_rot = ref 0 and b_show = ref false in
+	let range = Array.length Sys.argv in
+	for i = 1 to range do
+		match Sys.argv.(i) with
+			| "-r" -> if i + 1 < range then
+	  			begin
+	    			rotok := true;
+	    			a := int_of_string ( Sys.argv.(i+1) );
+	  			end
+			| "-s" -> b_show := true;
+			| _ -> diver := 1; 
+	done;
 	sdl_init();
-let img = Sdlloader.load_image Sys.argv.(1) in
-  let (w,h) = get_dims img in 
-  let img1 = Pretreatment.image2gray img in
-  let img2 = Pretreatment.average ( img1) 165 in
-  let img3 = Pretreatment.binarization (Pretreatment.average img1 140) 200 in
-  let a = ref 0 in
-  if !rotok  then
-	begin
-    a := !d_rot ;
+	let img = Sdlloader.load_image Sys.argv.(1) in
+	let (w,h) = get_dims img in 
+	let img1 = Pretreatment.image2gray img in
+	let img2 = Pretreatment.average ( img1) 165 in
+	let img3 = Pretreatment.binarization (Pretreatment.average img1 140) 200 in
+	if not !rotok then
+		a := Rotation.detect_angle img3 ;
+	let img4 = Rotation.rotate img3 !a in
+	let img5 = Recognition.recognition img4 in
+	let display = Sdlvideo.set_video_mode w h [`DOUBLEBUF] in
+	show img display;
+	wait_key ();
+	if !b_show then
+		begin
+		show img1 display;
+		wait_key ();
+		show img2 display;
+		wait_key ();
+		show img3 display;
+		wait_key ();
+		show img4 display;
+		wait_key ();
+		show img5 display;
+		wait_key ();
+		end
+
+	show img5 display;
+	wait_key ();
+	exit 0
 	end
-	else
-  a := Rotation.detect_angle img3;
-  
-  let img4 = Rotation.rotate img3 !a in
-  let img5 = Recognition.identifyChar img4 in
-  let display = Sdlvideo.set_video_mode w h [`DOUBLEBUF] in
-  show img display;
-  wait_key ();
-  if b_show then
-    begin
-      show img1 display;
-      wait_key ();
-      show img2 display;
-      wait_key ();
-      show img3 display;
-      wait_key ();
-      show img4 display;
-      wait_key ();
-      show img5 display;
-      wait_key ();
-    end
-      show img5 display;
-  wait_key ();
-  exit 0
-	end
-  let _ =  main ()
+
+let _ =  main ()
