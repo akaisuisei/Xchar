5542ff65bc23bd5615e95f8028f03eb77834e288
ClumsyApe <teaforthree@hotmail.fr>
2013-10-23 16:07:27 +0200

    Update recognition.ml

--- src/recognition.ml
+++ src/recognition.ml
@@ -4,14 +4,14 @@ let get_dims img =
 
 (*Retourne la valeur maximum d'une liste
   d'entiers.*)
-let max list =
+let rec max list a =
   begin
-    let maximum = ref 0 in
-      for i=0 to (List.length list) - 1 do
-        if !maximum < (List.nth list i) then
-          maximum := List.nth list i
-      done;
-      !maximum
+    match list with
+    	[] -> a
+    	|e::l -> if (a > e) then
+    		   max l a
+    		 else
+    		   max l e
   end
 
 (*Renvoie la copie d'une image.*)
@@ -53,24 +53,22 @@ let identifyLines img height list =
 (*Délimite par des colonnes colorées les caractères.*) (*Fonction de test.*)
 let identifyChar img abs ord height color =
   begin
-    let surface = ref (copyImg !img) in
     for i=ord to ord + height - 1 do
-      Sdlvideo.put_pixel_color !surface abs i color
-    done;
-    img := !surface
+      Sdlvideo.put_pixel_color img abs i color
+    done 
   end
 
 (*Détermine la position la plus à gauche
   d'un début de ligne dans le texte.*)
 let findStart img =
   begin
-    let (width, height) = get_dims !img
+    let (width, height) = get_dims img
     and i = ref 0
     and j = ref 0 
     and searching = ref true in
     while ((!i < width - 1) && (!searching = true)) do
       while ((!j < height - 1) && (!searching = true)) do
-        (if (Sdlvideo.get_pixel_color !img !i !j = Sdlvideo.black) then
+        (if (Sdlvideo.get_pixel_color img !i !j = Sdlvideo.black) then
 	  (searching := false)
 	else
 	  (j := !j + 1))
@@ -89,14 +87,14 @@ let normalizeH img =
     let list = ref []
     and list2 = ref []
     and inLine = ref false
-    and width = (Sdlvideo.surface_info !img).Sdlvideo.w
-    and height = (Sdlvideo.surface_info !img).Sdlvideo.h
+    and width = (Sdlvideo.surface_info img).Sdlvideo.w
+    and height = (Sdlvideo.surface_info img).Sdlvideo.h
     and countH = ref 0
     and whiteLine = ref 0 in
       for j=0 to height - 1 do
         for i=0 to width - 1 do
           if ((!inLine = false) 
-	    && (Sdlvideo.get_pixel_color !img i j = Sdlvideo.black)) then
+	    && (Sdlvideo.get_pixel_color img i j = Sdlvideo.black)) then
               (inLine := true;
 	      list2 := !list2 @ [j])
 	  else if ((!inLine = true)
@@ -109,7 +107,7 @@ let normalizeH img =
 	      else
 	          (countH := !countH + 1;
 	           whiteLine := 0))
-	  else if (Sdlvideo.get_pixel_color !img i j = Sdlvideo.white) then
+	  else if (Sdlvideo.get_pixel_color img i j = Sdlvideo.white) then
 	      (whiteLine := !whiteLine + 1)
         done
       done;
@@ -123,21 +121,21 @@ let normalizeW img maxH lines =
   begin
     let start = findStart img
     and list3 = ref []
-    and width = (Sdlvideo.surface_info !img).Sdlvideo.w
+    and width = (Sdlvideo.surface_info img).Sdlvideo.w
     and inChar = ref false
     and countW = ref 0 
     and whiteLine = ref 0 in
     for i=0 to (Array.length lines) - 1 do
       for abs=start to width - 1 do
 	for ord=lines.(i) to lines.(i) + maxH - 1 do
-	  if (Sdlvideo.get_pixel_color !img abs ord = Sdlvideo.black) then
+	  if (Sdlvideo.get_pixel_color img abs ord = Sdlvideo.black) then
 	    (if (!inChar = false) then
 		(inChar := true;
 	        identifyChar img (abs-1) lines.(i) maxH Sdlvideo.blue)
 	     else
 	       (if (ord = lines.(i) + maxH - 1) then
 		   countW := !countW + 1))
-          else if (Sdlvideo.get_pixel_color !img abs ord = Sdlvideo.white) then
+          else if (Sdlvideo.get_pixel_color img abs ord = Sdlvideo.white) then
 	    (if (ord = lines.(i) + maxH - 1) then
 		(if (!inChar = true) then
 		    (if (!whiteLine = maxH - 1) then
